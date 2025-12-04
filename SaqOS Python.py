d92bfd542fec6875076084fb091fcb673a17f76e
theresistance-12 <saqlainhasan615@gmail.com>
2025-12-04 20:26:04 +0100

    Add files via upload

---
+++ SaqOS Python.py
@@ -0,0 +1,78 @@
+user = input ("what is username?")
+passwrd = input ("what is password?")
+if user == ("saqlain"):
+    print ("Welcome to SaqOs Python Edition!")
+elif user == ("sameer"):
+    print ("Welcome to SaqOs Python Edition!")
+else:
+    print ("wrong password or username! you wont log in as root!")
+if passwrd == ("s"):
+    print ("Type help for all commands and questions")
+elif passwrd == ("s"):
+    print ("Type help for all commands and questions")
+else:
+    print ("wrong password or username you wont log in as root!")
+while True:
+    os = input ("SaqOs Py>>>")
+    if os == ("cd"):
+        print ("you are in the current dir sda 2 dev 3")
+    elif os == ("sudo"):
+        print ("you are the super user! if the prompt at logging in said: wrong password or username! you are not super user!")
+    elif os == ("exit"):
+        print ("shutting down...")
+        break
+    elif os == ("ls"):
+        print ("CURRENT FOLDER:saqos. FILES OF SAQOS:kernel.py,makefile,and grub.cfg")
+    elif os == ("neofetch"):
+        print (":-::_:SAQKERNELVERSIONPY:-::-.::ç(+=)(")
+    elif os == ("spam"):
+        for i in range(5):
+            print ("spam")
+    elif os == ("lag"):
+        for i in range(100):
+            print ("lag")
+    elif os == ("crash"):
+        for i in range(1000000):
+            print ("crash")
+    elif os == ("who is terry davis?"):
+        print ("he is one of the best programmer ever, he inspiered me to make SaqOS.")
+    elif os == ("what is 1+1?"):
+        print ("2")
+    elif os == ("help"):
+        print ("COMMANDS:cd,sudo,exit,ls,neofetch,spam,lag,crash,help. QUESTIONS:who is terry davis?,what is 1+1? APPS AND SOFTWARE INSTALLATIONS: sudo saq install steam, sudo saq install 32-libraries, sudo saq install GNU tools. More questions and commands will come in newer builds of SaqOs Py edition.")
+    elif os == ("rm -rf /"):
+        print ("Deleting saqKernel.... deleting Saq Tools....")
+        for i in range(1000000000000):
+            print ("ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED! ERROR! KERNEL PANIC! SYSTEM HALTED!")
+    elif os == ("sudo saq install steam"):
+        print ("Getting steam from saq reposetory... verifying package... installing steam... steam installed successfully! You may launch it by typing sudo steam, always start apps with sudo or else it will be an unknown command!")
+    elif os == ("sudo saq install 32-bit libraries"):
+        print ("Getting 32-bit libs... verifying packages... installing 32-bit libs... 32-bit libs installed successfully!")
+    elif os == ("sudo saq install GNU tools"):
+        print ("Getting GNU packages from GNU.org... verifying packages... installing GNU tools... GNU tools installed successfully!")
+    elif os == ("sudo saq install Linux"):
+        print ("INSTALLING LINUX WILL REPLACE THE CURRENT SAQOS KERNEL AND MAY BREAK EVERYTHING DO IT AT OWN RISK!!!!! Getting Linux Packages from kernel.org... fetched the newest 6.15.0 kernel installing Linux 6.15.0... Linux installed successfully!")
+        for i in range(10):
+            print ("THE SAQOS KERNEL IS NOT FOUND YOURE ON YOUR OWN. su->>>")
+            break
+    elif os == ("sudo steam"):
+        print ("Welcome to steam in SaqOs You may choose a game now make sure to install 32-bit libs")
+        while True:
+            steam = input ("What game you wanna start?")
+            if steam == ("half life"):
+                print ("Starting hl... godon freman saves city 17 you won")
+            elif steam == ("exit"):
+                print ("exiting...")
+                break
+    elif os == ("calc"):
+        print ("Welcome to Saqs Calc under the GNU's GPL licence! type exit to exit")
+        while True:
+            calc = input ("")  
+            num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+result = num1 + num2
+print("Result:", result)
+          
+
+                       
+    
